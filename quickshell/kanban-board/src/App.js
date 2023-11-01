cf151582cf1f02936095e3cc503f52d54f8d9a5b
Pranjhi <pranjhisingh2208@gmail.com>
2023-11-02 00:35:38 +0530

    kanban-board

---
+++ quickshell/kanban-board/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import KanbanBoard from './kanban';
+function App() {
+  return (
+    <div className="App">
+      <h1>Kanban Board App</h1>
+      <KanbanBoard/>
+    </div>
+  );
+}
+
+export default App;
